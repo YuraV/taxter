0b619198ec360c5a794334c236f2b9bd89decf34
Yurii Vodolyak <yurii.vodolyak@onapp.com>
2014-01-13 11:17:58 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taxter::Application.config.secret_token = '19f778ed4895920ffa760a1148386c4b71143fb640b36df8afac89990655c63f200f4d43b735a044e29f53cb2e74ac4558f29769da187df53d9e847e254e2e34'
